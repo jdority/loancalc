81b039eb1dedffddc8ad697a8faf5f4b08b497ee
JD <jdority@salesforce.com>
2021-10-07 12:56:50 -0700

    3 inputs

--- force-app/main/default/lwc/loancalc/loancalc.js
+++ force-app/main/default/lwc/loancalc/loancalc.js
@@ -1,24 +1,33 @@
 import { LightningElement, track } from 'lwc';
 export default class CalculatorInLwc extends LightningElement {
-    @track firstNumber;
-    @track secondNumber;
+    @track principalNumber;
+    @track interestNumber;
+    @track monthsNumber;
     resultValue;
-    handleNumberOeChange(event) {
-        this.firstNumber = parseInt(event.target.value);
+    handleNumberPChange(event) {
+        this.principalNumber = parseInt(event.target.value);
     }
-    handleNumberTwoChange(event) {
-        this.secondNumber = parseInt(event.target.value);
+    handleNumberIChange(event) {
+        this.interestNumber = parseInt(event.target.value);
     }
+    handleNumberMChange(event) {
+        this.monthsNumber = parseInt(event.target.value);
+    }
+
+
+
+
+
     addition() {
-        this.resultValue = parseInt(this.firstNumber) + parseInt(this.secondNumber);
+        this.resultValue = parseInt(this.principalNumber) + parseInt(this.interestNumber);
     }
     multification() {
-        this.resultValue = this.firstNumber * this.secondNumber;
+        this.resultValue = this.principalNumber * this.interestNumber;
     }
     subtraction() {
-        this.resultValue = this.firstNumber - this.secondNumber;
+        this.resultValue = this.principalNumber - this.interestNumber;
     }
     division() {
-        this.resultValue = this.firstNumber / this.secondNumber;
+        this.resultValue = this.principalNumber / this.interestNumber;
     }
 }
