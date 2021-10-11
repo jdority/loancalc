ab51d18d308b5b5820387f91e0d35488a48afb35
JD <jdority@salesforce.com>
2021-10-11 12:22:09 -0700

    fix calculation so correct # of payments show

--- force-app/main/default/lwc/loancalc/loancalc.js
+++ force-app/main/default/lwc/loancalc/loancalc.js
@@ -1,12 +1,45 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
+
 export default class CalculatorInLwc extends LightningElement {
     @track principalNumber;
     @track interestNumber;
     @track monthsNumber;
-    resultValue;
-    interestrate;
-    monthlyrate;
-    payment;
+
+    @api keyField = "number";
+    @api rows = [];
+    @api columns = [
+            {
+                label: 'Payment Number',
+                fieldName: 'number',
+                type: 'number'
+            },
+            {
+                label: 'Balance',
+                fieldName: 'balance',
+                type: 'currency'
+            },
+            {
+                label: 'Payment', 
+                fieldName: 'payment',
+                type: 'currency'
+            },
+            {
+                label: 'Interest',
+                fieldName: 'interest',
+                type: 'currency'
+            },
+            {
+                label: 'Principal',
+                fieldName: 'principal',
+                type: 'currency'
+            }
+        ];
+
+        resultValue;
+        interestrate;
+        monthlyrate;
+        payment;
+
 
     handleNumberPChange(event) {
         this.principalNumber = parseInt(event.target.value);
@@ -23,7 +56,11 @@ export default class CalculatorInLwc extends LightningElement {
         this.interestrate = this.interestNumber/100;
         let payments = this.amortize(this.principalNumber,this.interestrate,this.monthsNumber);
         console.log(payments);
-       
+        console.log('Eat Turkey');
+ 
+        this.rows = payments.slice();
+        console.log(this.rows);
+        
     }
 
     amortize(balance, interestRate, terms) {
@@ -35,20 +72,24 @@ export default class CalculatorInLwc extends LightningElement {
         for (var count = 0; count < terms; ++count)	{ 
             let paymentItem = {};
             
-                //in-loop interest amount holder
-                let interest = 0;
-                let paymentInterest = balance * monthlyRate;
-            let monthlyPrincipal = payment - interest;
+            //in-loop interest amount holder
+            let interest = 0;
+            let monthlyPrincipal = 0;
+
+            let paymentInterest = balance * monthlyRate;
+            monthlyPrincipal = payment - paymentInterest; //fixed (was interest)
+            
             
             paymentItem.number = count + 1;
             paymentItem.payment = payment.toFixed(2);
             paymentItem.balance = balance.toFixed(2);  
-                paymentItem.interest = paymentInterest.toFixed(2);
+            paymentItem.interest = paymentInterest.toFixed(2);
             paymentItem.principal = (payment - paymentInterest).toFixed(2);
             paymentList.push(paymentItem);
             
-                //update the balance for each loop iteration
-                balance = balance - monthlyPrincipal;		
+            //update the balance for each loop iteration
+            balance = balance - monthlyPrincipal;	
+            
         }
         return paymentList;
       }
